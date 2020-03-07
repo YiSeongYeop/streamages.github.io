df74c6b92ea266f3791797138015610bc32d98f2
YiSeongYeop <appleminto55@kaist.ac.kr>
2020-03-07 19:36:54 +0900

    erase

--- main.js
+++
@@ -1,9 +0,0 @@
-function enterSearch() {
-    if(event.keyCode == 13){
-        myFunction();  // 실행할 이벤트
-    }
-}
-function myFunction() {
-    var x = document.getElementById("bin").value;
-    window.location.href = "http://192.168.0.20/" + x + ".html";
-}
